2e66eb925656ef3c0afc28592f06a7172cce76a2
well done <vks7633a@gmail.com>
2025-03-16 12:58:46 +0530

    testing

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,47 +3,47 @@ const User=require('../models/user')
 exports.Signup = async (req, res) => {
     console.log("hjfdkjkmx")
     res.send("done");
-//   try {
-//     upload(req, res, async (err) => {
+  try {
+    upload(req, res, async (err) => {
     
-//       if (err) {
-//         return res
-//           .status(500)
-//           .json({ message: "File upload error", error: err });
-//       }
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "File upload error", error: err });
+      }
 
-//       if (!req.file) {
-//         return res.status(400).json({ message: "No file uploaded" });
-//       }
+      if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
 
-//       const {email,password,cpassword,languages,username}=req.body
+      const {email,password,cpassword,languages,username}=req.body
 
-//       const image= req.file ? req.file.path : null;
-//       console.log("this is image url",image)
+      const image= req.file ? req.file.path : null;
+      console.log("this is image url",image)
             
-//             if (!email || !password || !cpassword || !username) {
-//                 return res.status(400).json({ error: 'All fields are required.' });
-//             }
+            if (!email || !password || !cpassword || !username) {
+                return res.status(400).json({ error: 'All fields are required.' });
+            }
 
-//             if (password !== cpassword) {
-//                 return res.status(400).json({ error: 'Passwords do not match.' });
-//             }
+            if (password !== cpassword) {
+                return res.status(400).json({ error: 'Passwords do not match.' });
+            }
 
-//             // Save user to the database
-//             const user = new User({
-//                 username,
-//                 email,
-//                 password,
-//                 languages: languages ? languages.split(',') : [],
-//                 image,
-//             });
+            // Save user to the database
+            const user = new User({
+                username,
+                email,
+                password,
+                languages: languages ? languages.split(',') : [],
+                image,
+            });
 
-//             await user.save();
+            await user.save();
 
-//             res.status(201).json({ message: 'User registered successfully!', user });
+            res.status(201).json({ message: 'User registered successfully!', user });
 
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
